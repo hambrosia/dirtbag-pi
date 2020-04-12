5270f3a7eb4459c066af06a14ab7100b312fcc31
Matt Hambro <matthew.hambro@gmail.com>
2020-04-11 18:21:55 -0700

    Add comments and update correct units

--- api/api.py
+++ api/api.py
@@ -11,20 +11,24 @@ ss = Seesaw(i2c_bus, addr=0x36)
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-# SOIL READING CALIBRATION
+# Soil Reading Calibration
 MIN_MOISTURE = 315
 MAX_MOISTURE = 1015 - MIN_MOISTURE
+
+#Convert raw moisture reading to percent
 def get_soil_moisture_percent(raw_value: int) -> int:
     percent = ( (raw_value - MIN_MOISTURE ) / MAX_MOISTURE ) * 100
     return percent
+
+# Main endpoint
 @app.route('/', methods=['GET'])
 def home():
     soil_moisture_raw  = ss.moisture_read()
     soil_moisture_percent = round(get_soil_moisture_percent(soil_moisture_raw), 2)
     soil_temp = round(ss.get_temp(), 2)
     html = "<h1>DirtBag-Pi, at your service</h1>"
-    html += "<br>Soil Moisture: " + str(soil_moisture_percent) 
-    html += "<br>Soil Temp. C.: " + str(soil_temp)
+    html += "<br>Soil Moisture: " + str(soil_moisture_percent) + " Percent" 
+    html += "<br>Soil Temperature: " + str(soil_temp) + " Celcius"
     return html
 
 if __name__ == '__main__':
