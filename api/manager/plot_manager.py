f0bf0edaed34246670da2faeded284b5a4c15ef6
Matt Hambro <matthew.hambro@gmail.com>
2020-04-21 16:49:20 -0700

    Add temperature to graph

--- api/manager/plot_manager.py
+++ api/manager/plot_manager.py
@@ -1,8 +1,7 @@
 """Render html graph for index"""
 from datetime import datetime
 
-import plotly
-from plotly.graph_objs import Layout, Scatter
+import plotly.graph_objects as go
 
 import definitions
 import manager.db_manager as db_manager
@@ -19,12 +18,14 @@ def plot_month():
 
     timestamps = [row['timestamp'] for row in readings_last_month]
     moisture_readings = [sensor_manager.get_soil_moisture_percent(row['soilmoisture']) for row in readings_last_month]
+    temp_readings = [row['soiltemp'] for row in readings_last_month]
 
     root_path = definitions.ROOT_DIR
     output_path = str(root_path) + '/static/index.html'
-    plotly.offline.plot({
-        "data": [Scatter(x=timestamps, y=moisture_readings, mode='lines')],
-        "layout": Layout(title="DirtBag Pi: Soil Moisture Last 31 Days")},
-        filename=output_path)
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=timestamps, y=moisture_readings, mode='lines', name='Soil Moisture Percent'))
+    fig.add_trace(go.Scatter(x=timestamps, y=temp_readings, mode='lines', name='Soil Temperature Celcius'))
+    fig.write_html(output_path)    
 
     print("%s: Updated index.html" % timestamp_now)
