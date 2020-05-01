dba76fb73553d80c20720d2a25c479065a1f7bfb
Matt Hambro <matthew.hambro@gmail.com>
2020-05-01 10:13:47 -0700

    Update requirements.txt

--- dirtbag/cloud-hosted/client/api/app.py
+++ dirtbag/cloud-hosted/client/api/app.py
@@ -2,11 +2,11 @@
  """
 
 import manager.schedule_manager as schedule_manager
-
+import manager.sensor_manager as sensor_manager
 
 def main() -> None:
-    schedule_manager.on_startup()
-
+    soil_moisture = sensor_manager.get_soil_moisture()
+    print(soil_moisture)
 
 if __name__ == "__main__":
     main()
--- dirtbag/cloud-hosted/client/api/manager/schedule_manager.py
+++ dirtbag/cloud-hosted/client/api/manager/schedule_manager.py
@@ -12,17 +12,17 @@ SCHEDULER = BackgroundScheduler()
 def start_monitor():
     """ Start scheduler to update DB and rerender index on interval"""
     polling_interval = CONFIGS['polling-interval-minutes']
-    SCHEDULER.add_job(func="", trigger='interval', minutes=polling_interval)
+#    SCHEDULER.add_job(func="", trigger='interval', minutes=polling_interval)
 
     timestamp = datetime.now()
     print("%s: Scheduler configured to take and save reading every %i minutes" % (timestamp, polling_interval))
 
 
 def on_startup():
-    """Prepopulate database, prerender html, start scheduler"""
+    """Validate config.json and connection to API, take a reading, start scheduler"""
     timestamp = datetime.now()
-    print("%s: Prepopulating database with initial reading, prerendering index.html" % timestamp)
-
+    print("%s: Validating config.json and cnonection to API." % timestamp)
+    
     # Start monitor scheduler
-    start_monitor()
-    SCHEDULER.start()
+#    start_monitor()
+#    SCHEDULER.start()
