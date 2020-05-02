528316379c583f28a85066fff95817edc20f219c
Matt Hambro <matthew.hambro@gmail.com>
2020-05-01 18:25:23 -0700

    Remove API Gateway

--- dirtbag/cloud-hosted/infra/terraform/iam.tf
+++ dirtbag/cloud-hosted/infra/terraform/iam.tf
@@ -140,53 +140,6 @@ resource "aws_iam_role_policy_attachment" "render_index" {
   policy_arn = aws_iam_policy.render_index.arn
 }
 
-# API Gateway Permissions
-resource "aws_iam_role" "api_gateway_cloudwatch" {
-  name = "api_gateway_cloudwatch_global"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "apigateway.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "api_gateway_cloudwatch" {
-  name = "default"
-  role = aws_iam_role.api_gateway_cloudwatch.id
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:DescribeLogGroups",
-                "logs:DescribeLogStreams",
-                "logs:PutLogEvents",
-                "logs:GetLogEvents",
-                "logs:FilterLogEvents"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
-
 # Soil Sensor Client IAM Permissions
 resource "aws_iam_user" "dirtbag_client" {
   name = "dirtbag-client"
