7aa9705e088b8eb08584d6e9bf3bda75fce7ce86
Matt Hambro <matthew.hambro@gmail.com>
2020-10-13 22:34:07 -0700

    Comment out id provider

--- dirtbag/cloud-hosted/infra/terraform/modules/dirtbag-aws/cognito.tf
+++ dirtbag/cloud-hosted/infra/terraform/modules/dirtbag-aws/cognito.tf
@@ -7,13 +7,15 @@ resource "aws_cognito_user_pool" "cognito_user_pool" {
 
 }
 
+
+# TODO: Fix the identity, needs a client resource
 resource "aws_cognito_identity_pool" "cognito_id_pool" {
-  identity_pool_name = "${replace(var.instantiation_name, "-", "")}CognitoIdPool"
+  identity_pool_name = "dirtbag"
   allow_unauthenticated_identities = false
 
-  cognito_identity_providers {
-    client_id = aws_cognito_user_pool.cognito_user_pool.id
-    provider_name = aws_cognito_user_pool.cognito_user_pool.name
-    server_side_token_check = false
-  }
+//  cognito_identity_providers {
+//    client_id = aws_cognito_user_pool.cognito_user_pool.id
+//    provider_name = aws_cognito_user_pool.cognito_user_pool.name
+//    server_side_token_check = false
+//  }
 }
