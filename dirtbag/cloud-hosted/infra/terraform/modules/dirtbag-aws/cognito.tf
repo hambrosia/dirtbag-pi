7cf7cfd8b64fb90fd138739911e029b02956dd62
Matt Hambro <matthew.hambro@gmail.com>
2020-10-14 22:05:57 -0700

    Remove ID pool

--- dirtbag/cloud-hosted/infra/terraform/modules/dirtbag-aws/cognito.tf
+++ dirtbag/cloud-hosted/infra/terraform/modules/dirtbag-aws/cognito.tf
@@ -1,4 +1,4 @@
-resource "aws_cognito_user_pool" "cognito_user_pool" {
+resource "aws_cognito_user_pool" "dirtbag_user_pool" {
   name = "${var.instantiation_name}-cognito-user-pool"
 
   admin_create_user_config {
@@ -7,15 +7,8 @@ resource "aws_cognito_user_pool" "cognito_user_pool" {
 
 }
 
-
-# TODO: Fix the identity, needs a client resource
-resource "aws_cognito_identity_pool" "cognito_id_pool" {
-  identity_pool_name = "dirtbag"
-  allow_unauthenticated_identities = false
-
-//  cognito_identity_providers {
-//    client_id = aws_cognito_user_pool.cognito_user_pool.id
-//    provider_name = aws_cognito_user_pool.cognito_user_pool.name
-//    server_side_token_check = false
-//  }
+resource "aws_cognito_user_pool_client" "dirtbag_ui_app_client" {
+  name = "${var.instantiation_name}-ui-app-client"
+  user_pool_id = aws_cognito_user_pool.dirtbag_user_pool.id
 }
+
