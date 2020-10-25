2eab8516627bf0819dbff7bf70904b35da04e65a
Matt Hambro <matthew.hambro@gmail.com>
2020-10-25 14:51:53 -0700

    Enable link verification

--- dirtbag/cloud-hosted/infra/terraform/modules/dirtbag-aws/cognito.tf
+++ dirtbag/cloud-hosted/infra/terraform/modules/dirtbag-aws/cognito.tf
@@ -1,10 +1,16 @@
 resource "aws_cognito_user_pool" "dirtbag_user_pool" {
   name = "${var.instantiation_name}-cognito-user-pool"
 
-  admin_create_user_config {
-    allow_admin_create_user_only = true
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_LINK"
   }
 
+  auto_verified_attributes = ["email"]
+}
+
+resource "aws_cognito_user_pool_domain" "main" {
+  domain       = "${var.instantiation_name}-domain"
+  user_pool_id = aws_cognito_user_pool.dirtbag_user_pool.id
 }
 
 resource "aws_cognito_user_pool_client" "dirtbag_ui_app_client" {
