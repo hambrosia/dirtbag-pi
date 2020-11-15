1a01c7c1f21c5e7b0b363c5a0cd5dcb5568ce004
Matt Hambro <matthew.hambro@gmail.com>
2020-11-14 19:33:17 -0800

    Add id pool

--- dirtbag/cloud-hosted/infra/terraform/modules/dirtbag-aws/cognito.tf
+++ dirtbag/cloud-hosted/infra/terraform/modules/dirtbag-aws/cognito.tf
@@ -18,3 +18,13 @@ resource "aws_cognito_user_pool_client" "dirtbag_ui_app_client" {
   user_pool_id = aws_cognito_user_pool.dirtbag_user_pool.id
 }
 
+resource "aws_cognito_identity_pool" "dirtbag_ui_app_id_pool" {
+
+  identity_pool_name = "${var.instantiation_name}-id-pool"
+  allow_unauthenticated_identities = false
+  cognito_identity_providers {
+    client_id = aws_cognito_user_pool_client.dirtbag_ui_app_client.id
+    provider_name = aws_cognito_user_pool_client.dirtbag_ui_app_client.name
+    server_side_token_check = false
+  }
+}
